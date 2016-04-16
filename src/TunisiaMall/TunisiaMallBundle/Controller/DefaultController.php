8d1f818ace2f6396c49f8ced7c9ee05bba794a38
hakimnaas <abdelhakim.naas@esprit.tn>
2016-04-16 16:01:15 +0200

    fos_user

--- src/TunisiaMall/TunisiaMallBundle/Controller/DefaultController.php
+++ src/TunisiaMall/TunisiaMallBundle/Controller/DefaultController.php
@@ -8,6 +8,14 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('TunisiaMallBundle:Default:index.html.twig');
+         if ($this->has('security.csrf.token_manager')) {
+            $csrfToken = $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue();
+        } else {
+            // BC for SF < 2.4
+            $csrfToken = $this->has('form.csrf_provider')
+                ? $this->get('form.csrf_provider')->generateCsrfToken('authenticate')
+                : null;
+        }
+        return $this->render('TunisiaMallBundle:Default:index.html.twig',array('csrf_token'=>$csrfToken));
     }
 }
